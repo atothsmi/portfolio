fff093d4a8a7e919d7cf1c8d400dc87b38315630
atothsmi <atothsmi@ucsc.edu>
2024-02-06 10:45:38 -0800

    Thoughts Updates Cont 2

--- js/thoughts.js
+++ js/thoughts.js
@@ -1,19 +1,7 @@
-function getThoughts(file){
-    fetch(file).then((res) => {
-        if (!res.ok) {
-            throw new Error
-                (`HTTP error! Status: ${res.status}`);
-        }
-        return res.json();
-    })
-    .then((data) => {
-        for(let content in data){
-            $('#thoughts').append(content)
-        };
-    })
-    .catch((error) => 
-        console.error("Unable to fetch data:", error));
-};
 
-getThoughts('thoughtsHTML.json');
+$.getJSON('js\\thoughts.json', function(data) {
+    for(let content in data){
+        $('#thoughts').append(content)
+    };
+});
 
